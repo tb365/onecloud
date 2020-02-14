eb063a1740cc3fd3c299a8e4c84e87f48d0b6862
tangbin <tangbin@yunionyun.com>
2020-02-14 22:42:24 +0800

    huawei userdata fix

--- pkg/multicloud/huawei/instance.go
+++ pkg/multicloud/huawei/instance.go
@@ -1376,10 +1376,13 @@ func updateWindowsUserData(region *SRegion, userData string, imageId string, use
 	shells := ""
 	if isOldVersion {
 		shells += fmt.Sprintf("#ps1\n")
-		// 忽略错误
-		shells += fmt.Sprintf("$ErrorActionPreference= 'silentlycontinue'\n")
-		shells += fmt.Sprintf("net user %s %s /add\n", username, password)
-		shells += fmt.Sprintf("net localgroup administrators %s /add\n", username)
+		if username == "Administrator" {
+			shells += fmt.Sprintf("net user %s %s\n", username, password)
+		} else {
+			shells += fmt.Sprintf("net user %s %s  /add\n", username, password)
+			shells += fmt.Sprintf("net localgroup administrators %s  /add\n", username)
+		}
+
 		shells += fmt.Sprintf("net user %s /active:yes", username)
 	} else {
 		if !strings.HasPrefix(userData, "#ps1") {
